ff3feddc68fa9966f91cf9703c028382dedcfc09
DeadliestOne <uncountableaura@gmail.com>
2025-01-08 01:15:58 +0530

    Update hey.py

--- hey.py
+++ hey.py
@@ -32,6 +32,6 @@ def extract_upi_info(upi_id):
         return "Error occurred while fetching information"
 
 # Example usage
-upi_id = "dhruvx44@ibl"
+upi_id = "Anirban89@axl"
 result = extract_upi_info(upi_id)
 print(result)
